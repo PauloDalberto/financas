8ea59f24027ddcaaa84de5f626eb59d38d2bd425
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2024-09-16 09:23:44 -0300

    feat: add new content on about page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -2,8 +2,8 @@ import './about.css'
 
 export default function About(){
   return(
-    <main className="about-container">
-      <section className='about-infos about-container'>
+    <main >
+      <section className='about-container'>
         <img src="/foto-perfil.jpg" alt="Foto do criador do site" className='foto-perfil'/>
 
         <div className='presentation'>
@@ -12,10 +12,10 @@ export default function About(){
         </div>
       </section>
 
-      <section className='section'>
-        <h1>tese</h1>
+      <section className='about-infos'>
+        <div className='about-content'>
 
-        {/* Arrumar o fundo para ter o max-width correto e ver na pagina heaedr pq o border ta pegando 100%  e nao ta respeirando o paddigng */}
+        </div >
       </section>
       
     </main>
